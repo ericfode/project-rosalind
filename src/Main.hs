f6165c26614cc3354f851612ecfddf916a5fb599
School of Haskell <support@fpcomplete.com>
2013-09-23 14:12:19 +0000

    Remove show's, add comment pointing at the tutorial.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,31 @@
--- | Main entry point to the application.
+-- A complete discussion of this template can be found at
+-- https://www.fpcomplete.com/school/randoms
+
 module Main where
 
--- | The main entry point.
-main :: IO ()
+import System.Random
+import Data.List (foldl')
+
+-- Number of samples to take
+count = 10000
+
+-- Function to process our random sequence
+process :: [(Double, Double)] -> (Int, Int)
+process = foldl' sumInCircle (0, 0)
+
+-- Function to process a running value and a random value, producing a new running value.
+sumInCircle :: (Int, Int) -> (Double, Double) -> (Int, Int)
+sumInCircle (ins, total) (x, y) = (ins + if x*x + y*y < 1.0 then 1 else 0,
+                               total + 1)
+
+-- Function to display the running value.
+display:: (Int, Int) -> String
+display (heads, coins) = "π = "  ++ (show $ 4.0 * fromIntegral heads / fromIntegral coins)
+
+-- function to prepare the random sequence for processing
+prep :: [Double] -> [(Double, Double)]
+prep (a:b:r) = (a,b):prep r
+
 main = do
-    putStrLn "Welcome to FP Haskell Center!"
+  g <- newStdGen
+  putStrLn . display .process . take count . prep $ randoms g
